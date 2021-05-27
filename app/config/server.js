d7c8ff60434da67afa7403d79405b62a33150c47
Anh Le Tuan Tran <c.anh-le.tran@zonarsystems.com>
2021-05-27 16:04:03 +0700

    Init strapi app

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6b0332cb318b14dbd2c73f45af071cbf'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '33ab464b-6e47-43eb-9cb4-2a82f34fc530'
+};
